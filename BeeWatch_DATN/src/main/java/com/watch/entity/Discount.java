7d6e5a1c35ed92f344d110b35849d27894facc67
DESKTOP-I3T7RIV\acer <anhnqph27413@fpt.edu.vn>
2023-11-21 10:03:25 +0700

    qa sửa discount

--- BeeWatch_DATN/src/main/java/com/watch/entity/Discount.java
+++ BeeWatch_DATN/src/main/java/com/watch/entity/Discount.java
@@ -1,13 +1,10 @@
 package com.watch.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -25,6 +22,12 @@ public class Discount implements Serializable{
 	private int discountId;
 	private int percentDiscount;
 	private boolean status;
+
+	@Column(name = "begin_discount")
+	private Date beginDiscount;
+
+	@Column(name = "finish_discount")
+	private Date finishDiscount;
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "discount")
@@ -62,14 +65,31 @@ public class Discount implements Serializable{
 		this.categories = categories;
 	}
 
-	public Discount(int discountId, int percentDiscount, boolean status, List<Category> categories) {
-		super();
+	public Discount(int discountId, int percentDiscount, boolean status, Date beginDiscount, Date finishDiscount, List<Category> categories) {
 		this.discountId = discountId;
 		this.percentDiscount = percentDiscount;
 		this.status = status;
+		this.beginDiscount = beginDiscount;
+		this.finishDiscount = finishDiscount;
 		this.categories = categories;
 	}
-	
+
+	public Date getBeginDiscount() {
+		return beginDiscount;
+	}
+
+	public void setBeginDiscount(Date beginDiscount) {
+		this.beginDiscount = beginDiscount;
+	}
+
+	public Date getFinishDiscount() {
+		return finishDiscount;
+	}
+
+	public void setFinishDiscount(Date finishDiscount) {
+		this.finishDiscount = finishDiscount;
+	}
+
 	public Discount() {
 		// TODO Auto-generated constructor stub
 	}
