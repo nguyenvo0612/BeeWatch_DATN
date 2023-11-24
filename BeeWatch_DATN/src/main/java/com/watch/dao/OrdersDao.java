6dc8b7da89a501fa18467cd6b084b075d02d9603
nguyenvo0612 <ngvo.1203@gmail.com>
2023-11-24 17:25:51 +0700

    fix select thong ke

--- BeeWatch_DATN/src/main/java/com/watch/dao/OrdersDao.java
+++ BeeWatch_DATN/src/main/java/com/watch/dao/OrdersDao.java
@@ -18,23 +18,23 @@ import com.watch.entity.WishList;
 public interface OrdersDao extends JpaRepository<Orders, Integer>{
 
 	//chờ duyệt
-	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=1")
+	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=1 and Orders.sdtNn is not null and Orders.tenNn is not null and Orders.address is not null ")
 	Long choDuyet();
 
 	//đang xử lý
-	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=2")
+	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=2 and Orders.sdtNn is not null and Orders.tenNn is not null and Orders.address is not null")
 	Long dangXuLy();
 
 	//đang giao
-	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=3")
+	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=3 and Orders.sdtNn is not null and Orders.tenNn is not null and Orders.address is not null")
 	Long dangGiao();
 
 	//hoàn thành
-	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=4")
+	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=4 and Orders.sdtNn is not null and Orders.tenNn is not null and Orders.address is not null")
 	Long hoanThanh();
 
 	//đã  hủy
-	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=0")
+	@Query("select count(Orders.orderId) from Orders Orders where Orders.status=0 and Orders.sdtNn is not null and Orders.tenNn is not null and Orders.address is not null")
 	Long daHuy();
 
 	@Query("SELECT o FROM Orders o Where o.account.username=?1")
